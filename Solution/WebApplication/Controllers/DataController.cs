90dafc928e6721aad2626a30cb1239d222138417
JeongInHye <inhyea36@naver.com>
2019-02-08 13:49:47 +0900

    데이터 테스트

---
+++ Solution/WebApplication/Controllers/DataController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using WebApplication.Moudules;
+
+namespace WebApplication.Controllers
+{
+    public class DataController : Controller
+    {
+        private ArrayList list;
+        [Route("api/Select")]
+        [HttpGet]
+        public ArrayList Select()
+        {
+            list = new ArrayList();
+            DataBase db = new DataBase();
+            MySqlDataReader mdr = db.GetReader("SELECT * FROM Notice;");
+
+            while(mdr.Read())
+            {
+                Hashtable hashtable = new Hashtable();
+                for (int i = 0; i < mdr.FieldCount; i++)
+                {
+                    hashtable.Add(mdr.GetName(i), mdr.GetValue(i));
+                }
+                list.Add(hashtable);
+            }
+
+            mdr.Close();
+            db.ConnectionClose();
+
+            return list;
+        }
+    }
+}
