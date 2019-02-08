d35f5eb327759967f9e09df97733b8ea7026d9fe
JeongInHye <inhyea36@naver.com>
2019-02-08 13:54:02 +0900

    data 정보 변경

--- Solution/WebApplication/Moudules/DataBase.cs
+++ Solution/WebApplication/Moudules/DataBase.cs
@@ -23,7 +23,7 @@ namespace WebApplication.Moudules
         private bool GetConnection()
         {
             conn = new MySqlConnection();
-            conn.ConnectionString = string.Format("server={0};user={1};password={2};database={3};port={4}", "gdc3.gudi.kr", "root", "1234", "test", "27002");
+            conn.ConnectionString = string.Format("server={0};user={1};password={2};database={3};port={4}", "192.168.3.152", "root", "1234", "test", "3306");
 
             try
             {
